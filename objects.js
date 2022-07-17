5aa171c922580e2cec70c94be29a8ff3bbae0ddc
Juice <108875548+ariole1984@users.noreply.github.com>
2022-07-16 23:48:44 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,70 +1,76 @@
 
-// let person = {
-//    name: "ariole",
-//    lastName: "Dieujuste",
-//    age: 48,
-//    inClass: true,
-//    classesTaught: ['Javascript','arrays']
-// }
-//     console.log(person.name)
-//     console.log(person["name"])
-//     console.log(person["classesTaught"][0])
+let person = {
+   name: "ariole",
+   lastName: "Dieujuste",
+   age: 48,
+   inClass: true,
+   classesTaught: ['Javascript','arrays']
+}
+    console.log(person.name)
+    console.log(person["name"])
+    console.log(person["classesTaught"][0])
 
 
-    // let person = {
-    //     name: "ariole",
-    //     lastName: "Dieujuste",
-    //     age: 48,
-    //     inClass: true,
-    //     classesTaught: ['Javascript','arrays']
-    //  }
-    //     let {name, age} = person
-    //    console.log(name)
-    //    console.log(age)
+    let person = {
+        name: "ariole",
+        lastName: "Dieujuste",
+        age: 48,
+        inClass: true,
+        classesTaught: ['Javascript','arrays']
+       
+     }
+     
+    
+    
+    let {name, age} = person
+       console.log(name)
+       console.log(age)
          
-    //    person.name ="ariole" //edit
-    //    person.hasGlasses = true // add new property
-    //    console.log(person);
+       person.name ="ariole" //edit
+       person.hasGlasses = true // add new property
+       console.log(person);
 
-    //    person.Age = 21 // mistyped age.
-    //    console.log(person)
+       person.Age = 21 // mistyped age.
+       console.log(person)
 
-    // const allProps = Object.keys(person)
-    // console.log(allProps)
-    // const hasAge = allProps.indexOf("age") != -1
-    // console.log(hasAge)
+    const allProps = Object.keys(person)
+    console.log(allProps)
+    const hasAge = allProps.indexOf("age") != -1
+    console.log(hasAge)
 
     
 
 
 
 
-//     let person = {
-//         name: "Dragana",
-//         lastName: "Simonovska",
-//         age: 31,
-//         inClass: true,
+    let person = {
+        name: "Dragana",
+        lastName: "Simonovska",
+        age: 31,
+        inClass: true,
         
-//      }
+     }
 
-// console.log(person)
-// //change age to 2
-// person.age = 23
-// console.log(person)
+console.log(person)
+//change age to 2
+person.age = 23
+console.log(person)
 
 
-// let teacher = {
-//     name:"Darie",
-//     classes:[],
-//     currentlyTeaching: true,
-//     hairStyle: null,
-//  }
+
+
+let teacher = {
+    name:"Darie",
+    classes:[],
+    currentlyTeaching: true,
+    hairStyle: null,
+ }
      
-//     Teacher.Classes.push("array")
-//     teacher.classes.push("objects")
-//     cconsole.log("Teacher's name,Teachers. )
-//     console.log(classes)
-//     console.log(teacher)
+    Teacher.Classes.push("array")
+    teacher.classes.push("objects")
+    cconsole.log("Teacher's name,Teachers. )
+    console.log(classes)
+    console.log(teacher)
 
 
 
@@ -72,17 +78,17 @@
 
 
 
-// let TeacherAssistant = {
-//   name: "Juice",
-//   inClass: true,
+let TeacherAssistant = {
+  name: "Juice",
+  inClass: true,
   
 
-// }
+}
 
-//   console.log(TeacherAssistant)
-//   TeacherAssistant.Classes = []
-//   TeacherAssistant.Classes.push("array")
-//   console.log(TeacherAssistant)
+  console.log(TeacherAssistant)
+  TeacherAssistant.Classes = []
+  TeacherAssistant.Classes.push("array")
+  console.log(TeacherAssistant)
 
 let TeacherAssistants []
 
